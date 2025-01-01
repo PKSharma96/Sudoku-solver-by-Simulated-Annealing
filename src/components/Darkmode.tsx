8e6d13ea52dc8de699c3c459cf2f0f0fee4c784a
Pushpendra Kumar <pushpendrakr963.com>
2025-01-01 11:59:38 +0530

    First push of the Sudoku

--- src/components/Darkmode.tsx
+++ src/components/Darkmode.tsx
@@ -5,11 +5,11 @@ import { FaMoon } from "react-icons/fa";
 export default function Darkmode() {
   // Retrieve the theme from local storage, or default to "dark"
   const savedTheme = localStorage.getItem("theme");
-  const [theme, setTheme] = useState(savedTheme || "dark");
+  const [theme, setTheme] = useState(savedTheme || "light");
 
   useEffect(() => {
     // Update the class on the root element
-    document.documentElement.classList.toggle("dark", theme === "dark");
+    document.documentElement.classList.toggle("light", theme === "light");
 
     // Save the current theme to local storage
     localStorage.setItem("theme", theme);
@@ -23,6 +23,7 @@ export default function Darkmode() {
     <button
       onClick={switchTheme}
       className="text-gray-500 dark:text-gray-50 hover:text-gray-900 transition duration-300 dark:hover:text-yellow-500"
+      style={{ position: "absolute", right: "0", paddingRight: "5px" }}
     >
       {theme === "dark" ? <BsSunFill /> : <FaMoon />}
     </button>
